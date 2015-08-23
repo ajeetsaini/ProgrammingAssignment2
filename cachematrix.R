89035bd7fb7f1b2daf3d312a425f94e8308bd56e
ajeetsaini <ajeets@fractalanalytics.com>
2015-08-23 22:07:38 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,10 @@
 
 
+# makeCacheMatrix creates a list containing a function to
+# set the value of the matrix
+# get the value of the matrix
+# set the value of inverse of the matrix
+# get the value of inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
 inv <- NULL
 set <- function(y) {
@@ -11,8 +16,11 @@ setinverse <- function(inverse) inv <<- inverse
 getinverse <- function() inv
 list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
-
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
+# This function assumes that the matrix is always invertible.
 cacheSolve <- function(x, ...) {
 inv <- x$getinverse()
 if(!is.null(inv)) {
